5782125025ce21df2c326d68cc063522b5cfc110
illia <bronewi4ok@gmail.com>
2019-04-28 21:00:05 +0300

    yeah, i fixed it! google auth works!

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,6 +133,10 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-SITE_ID = 1
+SITE_ID = 2
 
 LOGIN_REDIRECT_URL = 'home'
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+
