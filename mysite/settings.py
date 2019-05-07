7ae28d71991fb981c5c9ed522c7581e152809ba6
illia <bronewi4ok@gmail.com>
2019-05-08 01:43:11 +0300

    try to fix bug with database

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,15 +81,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogdata',
-        'USER': 'illia',
-        'PASSWORD': '01021992_Random',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'blogdata',
+#         'USER': 'illia',
+#         'PASSWORD': '01021992_Random',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
